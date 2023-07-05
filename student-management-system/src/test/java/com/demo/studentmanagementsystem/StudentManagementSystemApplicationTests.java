bf2174128d8ad4a62dc5265cedaba5e333e3df30
Shane <reylandshane0069@gmail.com>
2023-07-05 08:20:52 +0800

    initialize spring boot

---
+++ student-management-system/src/test/java/com/demo/studentmanagementsystem/StudentManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.studentmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
